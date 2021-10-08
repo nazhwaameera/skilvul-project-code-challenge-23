5b411e3340c791ae98d0336860966dd6f9e7c3f6
revydo <eldodo641@gmail.com>
2021-10-08 15:40:22 +0700

    edit index

--- template/index.js
+++ template/index.js
@@ -1,6 +1,6 @@
-import { getPosts, getRandomPic } from './helpers.js';
+import { getPosts, getRandomPic } from "./helpers.js";
 
-const elDaftarBerita = document.querySelector('#daftar-berita');
+const elDaftarBerita = document.querySelector("#daftar-berita");
 
 /**
  *
@@ -9,22 +9,22 @@ const elDaftarBerita = document.querySelector('#daftar-berita');
  * @returns DOM Element
  */
 const createPostElement = (thumbnail, post) => {
-  const elCol = document.createElement('div');
-  const elCard = document.createElement('div');
-  const elCardImg = document.createElement('img');
-  const elCardBody = document.createElement('div');
-  const elCardTitle = document.createElement('h5');
-  const elCardBtn = document.createElement('a');
+  const elCol = document.createElement("div");
+  const elCard = document.createElement("div");
+  const elCardImg = document.createElement("img");
+  const elCardBody = document.createElement("div");
+  const elCardTitle = document.createElement("h5");
+  const elCardBtn = document.createElement("a");
 
-  elCol.classList.add('col-xs-12', 'col-sm-6', 'col-md-4', 'col-lg-3');
-  elCard.classList.add('card', 'w-100', 'my-3');
-  elCardImg.classList.add('card-img-top');
-  elCardBody.classList.add('card-body');
-  elCardTitle.classList.add('card-title');
-  elCardBtn.classList.add('btn', 'btn-primary', 'w-100', 'stretched-link');
+  elCol.classList.add("col-xs-12", "col-sm-6", "col-md-4", "col-lg-3");
+  elCard.classList.add("card", "w-100", "my-3");
+  elCardImg.classList.add("card-img-top");
+  elCardBody.classList.add("card-body");
+  elCardTitle.classList.add("card-title");
+  elCardBtn.classList.add("btn", "btn-primary", "w-100", "stretched-link");
 
-  elCardImg.setAttribute('alt', 'skilvul');
-  elCardBtn.innerHTML = 'Read More';
+  elCardImg.setAttribute("alt", "skilvul");
+  elCardBtn.innerHTML = "Read More";
 
   elCardBody.appendChild(elCardTitle);
   elCardBody.appendChild(elCardBtn);
@@ -33,12 +33,20 @@ const createPostElement = (thumbnail, post) => {
   elCol.appendChild(elCard);
 
   // EDIT HERE
-
+  elCardTitle.innerHTML = post.title;
+  elCardImg.setAttribute("src", thumbnail);
+  elCardBtn.setAttribute("href", `/post.html?post_id=${post.id}`);
   return elCol;
 };
 
 const renderPosts = async () => {
   // EDIT HERE
+  let posts = await getPosts();
+  console.log(posts);
+  let img = await getRandomPic();
+  Array.from(posts).forEach((data) => {
+    elDaftarBerita.appendChild(createPostElement(img, data));
+  });
 };
 
 renderPosts();
